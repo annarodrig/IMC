71d7cd48026d299ef8faa062ff34216a4c06d02b
= <=>
2022-04-10 18:16:05 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double  altura, peso, imc;
+
+Console.Write("Altura(m)..:");
+altura = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Peso(kg)...:");
+peso = Convert.ToDouble (Console.ReadLine());
+
+imc = peso / Math.Pow(altura, 2);
+
+Console.WriteLine($"IMC:{imc:N2}kg/m²");
